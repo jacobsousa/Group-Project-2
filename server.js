9e1a2a50137066362010745c19836700d467a79a
sufi <sufiyanshaikh11919@gmail.com>
2022-07-02 21:03:49 -0700

    added a user-model

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(port, () => console.log('Now listening'));
+});
